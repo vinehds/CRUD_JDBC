4c9ae2da148a56fa0cef2687d9a9b96ffb3f767e
Vinicius Dalaqua <viniciushdalaqua@gmail.com>
2024-11-04 12:30:46 -0300

    logonPage and entity User created

---
+++ src/main/java/model/entities/User.java
@@ -0,0 +1,86 @@
+package model.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String cpf;
+    private String email;
+    private String password;
+    private Date dateOfBirth;
+
+    public User() {
+    }
+
+    public User(String name, String cpf, String email, String password, Date dateOfBirth) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(cpf, user.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpf);
+    }
+}
